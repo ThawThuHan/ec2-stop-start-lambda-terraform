e48501b73430857de7f190690039d17683decef2
ThawThuHan <thawthuhan9@gmail.com>
2024-06-05 01:35:07 +0630

    add some output and variable

--- output.tf
+++ output.tf
@@ -1,7 +1,15 @@
-# output "aws_instance_id" {
-#   value = aws_instance.web.id
-# }
+output "cloudwatch_event_rule_start_id" {
+  value = aws_cloudwatch_event_rule.start_ec2_rule.id
+}
 
-# output "aws_instance_public_ip" {
-#   value = aws_instance.web.public_ip
-# }
+output "cloudwatch_event_rule_stop_id" {
+  value = aws_cloudwatch_event_rule.stop_ec2_rule.id
+}
+
+output "lambda_start_function_name" {
+  value = aws_lambda_function.start_ec2.function_name
+}
+
+output "lambda_stop_function_name" {
+  value = aws_lambda_function.stop_ec2.function_name
+}
